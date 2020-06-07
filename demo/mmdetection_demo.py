a9d0a1ff977f89b5d3e7ce32678c9cfae7c0f422
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MacBook-Pro.local>
2020-06-08 00:04:00 +0200

    print mask

--- demo/mmdetection_demo.py
+++ demo/mmdetection_demo.py
@@ -133,7 +133,7 @@ def process_jpg_crcnn(config_file, checkpoint_file, image_dir):
         bbox_result, segm_result = result
         segm_result = segm_result[0]
 
-
+        print(segm_result.shape)
         print (np.where(segm_result==True))
 
         bboxes = np.vstack(bbox_result)
