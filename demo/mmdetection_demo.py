e2c34791742d9eda2625ee6b0ff4da7205c9e39d
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MBP.fritz.box>
2020-06-08 12:01:31 +0200

    test size

--- demo/mmdetection_demo.py
+++ demo/mmdetection_demo.py
@@ -157,11 +157,14 @@ def process_jpg_crcnn(config_file, checkpoint_file, image_dir):
         for row in range(frame.shape[0]):
             for col in range(frame.shape[1]):
 
-                if mask[row][col] == True:
-                    mask_img[row][col] = 255
+                if mask[row][col] == False:
+                    frame[row][col+0] = 0
+                    frame[row][col+1] = 0
+                    frame[row][col+2] = 0
+                    # mask_img[row][col] = 255
 
         # res = cv2.bitwise_and(frame, frame, mask = np.float32(mask))
-        cv2.imwrite('res_mask.jpg', mask_img)
+        cv2.imwrite('res_mask.jpg', frame)
 
         # for i in range(len(bboxes)):
         #     bb = bboxes[i]
