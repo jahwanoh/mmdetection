076f0cdc6ed827869f17333a3f335c959c5ca6d6
bepro <jahwan.oh@bepro11.com>
2020-08-11 10:40:06 +0200

    update modified model config, add setup script

---
+++ setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+####
+#activate virtualenv
+
+pip install torch==1.3.1 torchvision==0.4.2
+pip install cython
+pip install pycocotools==2.0.0
+pip install mmcv==0.6.2
+
+# pip install -v -e .
+
+mkdir data
+ln -s $COCO_ROOT data
+
+gsutil -m cp gs://bepro-server-storage/dsort_tracking_data/installation/nccl-repo-ubuntu1804-2.6.4-ga-cuda10.0_1-1_amd64.deb .
+sudo dpkg -i nccl-repo-ubuntu1804-2.6.4-ga-cuda10.0_1-1_amd64.deb
+sudo apt update
+sudo apt install libnccl2=2.4.8-1+cuda10.0 libnccl-dev=2.4.8-1+cuda10.0
+
+gsutil -m cp gs://bepro-server-storage/dsort_tracking_data/checkpoints/crcnn_r50_bepro_stitch.pth .
+
+mkdir demo/dump
+
+## nvcc fatal : Unsupported gpu architecture 'compute_75'
+# export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}^C
+# export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+python setup.py develop
+
+cp -r configs ~/bepro-video-util 
+cp crcnn_r50_bepro_stitch.pth ~/bepro-video-util
+
+## test with
+# python demo/mmdetection_demo.py PVO4R8Dh-trim.mp4 configs/cascade_rcnn/cascade_rcnn_r50_fpn_1x_bepro.py crcnn_r50_bepro_stitch.pth 0 1000 ./tmp/
